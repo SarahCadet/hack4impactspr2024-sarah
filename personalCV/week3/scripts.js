1124d9de3ddd3fa7d542290b5894d05d19cc692a
Sarah Cadet <scadet@bu.edu>
2024-02-27 18:55:27 -0500

    changed name and printing the projects

--- personalCV/week3/scripts.js
+++ personalCV/week3/scripts.js
@@ -1,1 +1,7 @@
 console.log("Hello World");
+//print out all of the projects
+const projects = document.querySelector(".project");
+console.log(projects);
+//changing the name
+const myName = document.querySelector("#my-name");
+myName.textContent = "Sarah Cadet";
