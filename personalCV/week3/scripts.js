b0ac6b6a846b3f277b0d857dab6617b7add568ee
Sarah Cadet <scadet@bu.edu>
2024-02-27 19:21:25 -0500

    added functionality to buttons and learned how to apply listeners to elements that are selected by class

--- personalCV/week3/scripts.js
+++ personalCV/week3/scripts.js
@@ -5,3 +5,31 @@ console.log(projects);
 //changing the name
 const myName = document.querySelector("#my-name");
 myName.textContent = "Sarah Cadet";
+let numberOfClicks = 0;
+function changeNameColorBasedOnParity(){
+
+  numberOfClicks++;
+  if(numberOfClicks%2 == 0){
+    myName.style.color = "blue";
+  } else{
+    myName.style.color = "orange";
+  }
+}
+myName.addEventListener("click", changeNameColorBasedOnParity);
+//getting elements by Class Name returns an array/list of elements. 
+//Have to loop through all of them
+const buttons = document.getElementsByClassName("buttonGithub");
+for(let i = 0; i < buttons.length; i++){
+  buttons[i].addEventListener("click", function () {
+    window.open("https://github.com");
+  });
+}
+const linkedinImg = document.querySelector("#linkedin-img");
+linkedinImg.addEventListener("click", function(){
+window.open("https://linkedin.com");
+});
+
+const gitHubImg = document.querySelector("#github-img");
+gitHubImg.addEventListener("click", function(){
+window.open("https://github.com");
+});
